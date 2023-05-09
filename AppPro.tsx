c8cde2d282479d623b0078f660a0c966320a42a7
Samuel Asiabaka <samuelasiabaka@gmail.com>
2023-05-09 19:05:50 +0100

    set up

---
+++ AppPro.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  SafeAreaView,
+} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+        Hello World !
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  whiteText: {
+    color: '#ffffff',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+
+export default AppPro;
